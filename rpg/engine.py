507c1d50e6590950b257d1a8961d5a8a1697d3ef
Pinkysha228 <oleksijborsulak@gmail.com>
2025-04-24 14:09:43 +0300

    add class World and basic functions

--- rpg/engine.py
+++ rpg/engine.py
@@ -1,11 +1,12 @@
 import pygame
+from pytmx import load_pygame
+import json
 
 pygame.init()
 
 
 class Player:
-    def __init__(self, player_surf, hp, speed, screen):
-        self.player_surf_path = player_surf  # сохраняем путь
+    def __init__(self, hp, speed, screen):
         self.speed = speed
         self.hp = hp
         self.screen = screen
@@ -56,8 +57,7 @@ class Player:
         }
 
     def init_player(self):
-        self.player_surf = pygame.transform.rotozoom(pygame.image.load(self.player_surf_path).convert_alpha(), 0, self.scale)
-
+        self.player_surf = self.player_animations['idle'][0]  # <-- Додано це!
         self.player_rect = self.player_surf.get_rect(center=(400, 300))
 
     def show(self):
@@ -69,10 +69,6 @@ class Player:
         self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
         self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
 
-        # нормализуем движение по диагонали
-        # if self.direction.length_squared() > 0:
-        #     self.direction = self.direction.normalize()
-
         self.player_rect.center += self.direction * self.speed * dt
 
         if self.direction.x > 0:
@@ -83,13 +79,11 @@ class Player:
             self.lookDirection = 'down'
         elif self.direction.y < 0:
             self.lookDirection = 'up'
-
         elif self.direction.y == 0 and self.direction.x == 0:
             self.lookDirection = 'idle'
 
         if self.direction.length_squared() > 0:
             self.update_animation(dt)
-
         else:
             self.frame_index = 0
             self.player_surf = self.player_animations[self.lookDirection][0]
@@ -103,4 +97,34 @@ class Player:
                 self.frame_index = 0
         try:
             self.player_surf = self.player_animations[self.lookDirection][self.frame_index]
-        except IndexError: pass
+        except IndexError:
+            pass
+
+    def get_player_info(self):
+        player_info = {'x': self.direction.x, 'y': self.direction.y, 'hp': self.hp}
+        return player_info
+
+class World:
+    def __init__(self, map, screen, player):
+        self.map = map
+        self.screen = screen
+        self.tmx_data = None
+        self.player = player
+
+
+    def load_world(self):
+        self.tmx_data = load_pygame(self.map)
+    
+    def update_world(self):
+        for layer in self.tmx_data.visible_layers:
+            if hasattr(layer, "tiles"):
+                for x, y, image in layer.tiles():
+                    self.screen.blit(image, (x * self.tmx_data.tilewidth, y * self.tmx_data.tileheight))
+
+    def save_world(self, player_info):
+        save = {'player_dir_x': player_info['x'],
+            'player_dir_y': player_info['y'],
+            'player_hp': player_info['hp']}
+        print(save)
+        with open('save.json', 'w') as file:
+            json.dump(save, file, indent=4)
